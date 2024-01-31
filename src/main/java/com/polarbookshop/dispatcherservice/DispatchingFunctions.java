37717029ec25bf076d0a0da68cbd4aca01ef2dcf
codingkiddo <codingkiddo@gmail.com>
2024-01-31 08:33:46 +0530

    before start ch-11

--- src/main/java/com/polarbookshop/dispatcherservice/DispatchingFunctions.java
+++ src/main/java/com/polarbookshop/dispatcherservice/DispatchingFunctions.java
@@ -11,23 +11,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DispatchingFunctions {
-
 	private static final Logger log = LoggerFactory.getLogger(DispatchingFunctions.class);
-
-	@Bean
-	public Function<OrderAcceptedMessage, Long> pack() {
+    @Bean
+    Function<OrderAcceptedMessage, Long> pack() {
 		return orderAcceptedMessage -> {
 			log.info("The order with id {} is packed.", orderAcceptedMessage.orderId());
 			return orderAcceptedMessage.orderId();
 		};
 	}
-
-	@Bean
-	public Function<Flux<Long>, Flux<OrderDispatchedMessage>> label() {
+    @Bean
+    Function<Flux<Long>, Flux<OrderDispatchedMessage>> label() {
 		return orderFlux -> orderFlux.map(orderId -> {
 			log.info("The order with id {} is labeled.", orderId);
 			return new OrderDispatchedMessage(orderId);
 		});
 	}
-
 }
